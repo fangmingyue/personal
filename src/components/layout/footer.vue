7a0500840e6546874556252515ffcdc008d46cd8
fangmingyue <fangmingyue@gmail.com>
2024-12-04 19:42:29 +0800

    init final

---
+++ src/components/layout/footer.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template lang="pug">
+  #Footer
+    div this is footer
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/components/layout/header.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template lang="pug">
+  #Header
+    div this is header
+</template>
+
+<style lang="scss" scoped></style>
