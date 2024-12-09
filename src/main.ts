d26d506e5c1e20dffefb6e38913d9efab2c0185b
fangmingyue <fangmingyue@gmail.com>
2024-12-09 17:20:56 +0800

    update12090520

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,7 @@ import router from "./router";
 import "element-plus/dist/index.css";
 import "./style/index.css";
 import "./assets/css/main.css";
+import "./assets/scss/index.scss"
 
 const app = createApp(App);
 app.use(ElementPlus);
